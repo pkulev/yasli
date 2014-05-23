6f6656920dc8ca48b0d4611eb8b806f53cc1316f
pankshok <kulyov.pavel@gmail.com>
2014-05-23 19:16:34 +0400

    add *

---
+++ IML/iml.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import sys
+from iml_lexer import *
+
+if __name__ == '__main__':
+    try:
+        filename = sys.argv[1]
+    except Exception as e:
+        print e
+
+    file = open(filename)
+    src = file.read()
+    file.close()
+    tokens = iml_lex(src)
+    from pprint import pprint
+    pprint(tokens)
---
+++ IML/lexer.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+def lex(source, token_exprs):
+    pos = 0
+    tokens = []
+    end = len(source)
+    while pos < end:
+        match = None
+        for token_expr in token_exprs:
+            pattern, tag = token_expr
+            regex = re.compile(pattern)
+            match = regex.match(source, pos)
+            if match:
+                text = match.group(0)
+                if tag:
+                    token = (text, tag)
+                    tokens.append(token)
+                break
+        if not match:
+            sys.stderr.write('Illegal character {s}'.format(source[pos]))
+            sys.exit(1)
+        else:
+            pos = match.end(0)
+    return tokens
