c8d5bbc8156750f87ac5fbb8d0d75798a8527fbf
pankshok <kulyov.pavel@gmail.com>
2014-05-23 19:07:59 +0400

    minor changes

--- yasli_test.py
+++ yasli_test.py
@@ -7,4 +7,9 @@ def test_numbers():
     assert Inp().eval('42') == [42]
     assert Inp().eval('42 12') == [42, 12]
 
+def test_variables():
+#    assert Inp().eval('a') == [42]
+    assert Inp().eval({'a': 42}).eval('a') == [42]
+
 test_numbers()
+test_variables()
