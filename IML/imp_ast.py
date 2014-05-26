44bf0653eedc39f67da513ecd50ff41759c6632a
pankshok <kulyov.pavel@gmail.com>
2014-05-26 17:05:25 +0400

    add AST, equality and minor changes

---
+++ IML/imp_ast.py
@@ -0,0 +1,81 @@
+from equalty import *
+from IML.equality import Equality
+
+
+class Statement(Equality):
+    pass
+
+
+class Aexp(Equality):
+    pass
+
+
+class Bexp(Equality):
+    pass
+
+
+class AssignStatement(Statement):
+    def __init__(self, name, aexp):
+        self.name = name
+        self.aexp = aexp
+
+    def __repr__(self):
+        return 'AssignStatement({0}, {1})'.format(self.name, self.aexp)
+
+    def eval(self, env):
+        value = self.aexp.eval(env)
+        env[self.name] = value
+
+
+class CompoundStatement(Statement):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def __repr__(self):
+        return 'CompoundStatement({0}, {1})'.format(self.first, self.second)
+
+    def eval(self, env):
+        self.first.eval(env)
+        self.second.eval(env)
+
+
+class IfStatement(Statement):
+    def __init__(self, condition, true_stmt, false_stmt):
+        self.condition = condition
+        self.true_stmt = true_stmt
+        self.false_stmt = false_stmt
+
+    def __repr__(self):
+        return 'IfStatement({0}, {1}, {2})'.format(self.condition, self.true_stmt, self.false_stmt)
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        if condition_value:
+            self.true_stmt.eval(env)
+        else:
+            if self.false_stmt:
+                self.false_stmt.eval(env)
+
+
+class WhileStatement(Statement):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+    def __repr__(self):
+        return 'WhileStatement({0}, {1})'.format(self.condition, self.body)
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        while condition_value:
+            self.body.eval(env)
+            condition_value = self.condition.eval(env)
+
+
+class IntAexp(Aexp):
+    def __init__(self, i):
+        self.i = i
+
+    def __repr__(self):
+        return 'IntAexp({0})'.format(i)
